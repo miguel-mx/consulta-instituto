41e170aba190d62b1207449dd8fef644737bf899
Miguel Angel Magaña <miguel@matmor.unam.mx>
2022-09-06 10:09:20 -0500

    Prod

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    //.setManifestKeyPrefix('/build')
 
     /*
      * ENTRY CONFIG
@@ -71,6 +71,20 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+
+    .copyFiles({
+         from: './assets/images',
+
+         // optional target path, relative to the output dir
+         to: 'images/[path][name].[ext]',
+
+         // if versioning is enabled, add the file hash too
+         //to: 'images/[path][name].[hash:8].[ext]',
+
+         // only copy files matching this pattern
+         //pattern: /\.(png|jpg|jpeg)$/
+     })
 ;
 
 module.exports = Encore.getWebpackConfig();
